2b3f2489fbb1b9ce5b2bf14782d67996da623acc
ngaude <oktopela@yahoo.fr>
2014-12-11 23:50:29 +0100

    pairreads genome assembling
    graph DFS walk to enumerate every single eulerian path possible then
    filtered by reconstruction stage via pair-read.

--- bioinformatics-002/bioinformatics_chapter4.py
+++ bioinformatics-002/bioinformatics_chapter4.py
@@ -6,7 +6,7 @@ Created on Mon Dec 08 22:45:00 2014
 """
 
 from itertools import product
-
+import copy
 
 def composition(k, text):
     '''
@@ -19,6 +19,11 @@ def composition(k, text):
     for i in range(len(text) - k + 1):
         kmers.append(text[i:i+k])
     return sorted(kmers)
+    
+def pair_composition(k, d, text):
+    kmers = composition(2*k+d,text)
+    pairs = map(lambda  e: (e[:k],e[-k:]), kmers)
+    return pairs
 
 def genome_path(path):
     '''
@@ -123,6 +128,104 @@ def debruijn_from_kmer(kmers):
         g.append( (k,sorted(dprefix[k])) )
     return g
 
+
+
+def all_eularian_cycles(adjacency_list):
+    #build adjacency dict from adjacency list
+    original_graph = {}
+    for (k, v) in adjacency_list:
+        original_graph[k] = original_graph.get(k,[]) + v[:]
+         
+    def iterate_cgc_incremental_build(cgc):
+        # incremental build of cycle-graph-closed given cycle,graph
+        # this returns all possible incremental step
+        (cycle, graph, over) = cgc
+        if (over == True):
+            return [(cycle,graph,True)]
+        queue = cycle[-1]
+        nnode_list = graph.get(queue,[])
+        if (not nnode_list):
+            # if there is no option, return same, no update, it's over
+            return [(cycle,graph,True)]
+        elif len(nnode_list) == 1:
+            # if there is only one option do not copy struct
+            # add the single node option and update graph
+            nnode = nnode_list[0]
+            graph.pop(queue)
+            cycle.append(nnode)
+            closed = (cycle[0] == cycle[-1])
+            return [(cycle,graph,closed)]
+        else:
+            # there are multiple options from here...
+            cg = []
+            for nnode in nnode_list:
+                agraph = copy.deepcopy(graph)
+                annode_list = agraph.get(queue,[])
+                annode_list.remove(nnode)
+                acycle = cycle[:]
+                acycle.append(nnode)
+                closed = (cycle[0] == cycle[-1])
+                cg.append((acycle,agraph,closed))
+            return cg
+        
+    def inner_cycles(node,graph):
+        '''
+        return all possible cycles from node-node with its update unvisited edges
+        graph
+        '''
+        # init the cycles with a single node
+        lcgc = [(node,copy.deepcopy(graph),False)]
+        while True:
+            closed = True
+            update_lcgc = []
+            for cgc in lcgc:
+                update_lcgc += iterate_cgc_incremental_build(cgc)
+            for cgc in update_lcgc:
+                closed &= cgc[2]
+            if closed == True:
+                # hourray, all cycles from node have been built
+                return map(lambda e: (e[0], e[1]), update_lcgc)
+            else:
+                lcgc = update_lcgc
+        
+
+    def expand_cycles(cycle,graph):
+        # search for all expandable node given graph
+        lcg = []
+        for n,v in enumerate(cycle):
+            if v in graph:
+                prefix = cycle[:n]
+                suffix = cycle[n+1:]
+#                print 'cycle',cycle,'expand',v,'id',n
+#                print 'prefix',prefix
+#                print 'icycle',icycle
+#                print 'suffix',suffix
+#                print '--------------'
+                lcg +=  map(lambda (c,g) : (prefix+c+suffix,g), inner_cycles([v],graph))
+        return lcg
+
+    init_cycle = [original_graph.keys()[0]]
+    init_graph = copy.deepcopy(original_graph)
+    candidate = expand_cycles(init_cycle, init_graph)
+    solution = []
+    
+    while candidate:
+#        print 'candidate',candidate
+#        print 'len',len(candidate)
+        for (c,g) in candidate[:]:
+            candidate.remove( (c,g) )
+            if (not g):
+                # c is a cycle with an empty associated graph 
+                # e.g. it is a solution
+                if c[0]==c[-1]:
+                    solution.append(c)
+            else:
+                # graph is non empty let's expand another subcycles
+                candidate += expand_cycles(c,g)
+    
+    return solution    
+
+
 def eulerian_cycle(adjacency_list):
     #build adjacency dict from adjacency list
     dadj = {}
@@ -218,7 +321,8 @@ def nearly_balanced(adjacency_list):
     return edge that will balance perfectly 
     given graph assumed to be nearly balanced
     Input : nearly balanced graph
-    Output : balancing_edge
+    Output :
+    balancing_edge
     '''
     ind = {}
     outd = {}
@@ -264,13 +368,89 @@ def genome_reconstruction(kmers):
     '''
     return genome_path(eulerian_path(debruijn_from_kmer(kmers)))
 
+def genomes_reconstruction_from_pair(k, d, pairs):
+    '''
+    Solve the String Reconstruction from Read-Pairs Problem.
+    Input: Integers k and d followed by a collection 
+    of paired k-mers PairedReads.
+    Output: A string Text with (k, d)-mer composition 
+    equal to PairedReads.
+    '''
+    g = debruijn_from_pair(d,pairs)
+    e = nearly_balanced(g)
+    cycles = all_eularian_cycles(g + [( e[0], [e[1]] )])
+    #locate precisely the edge e transition within the cycle 
+    # to unwrap a path candidate
+    path = []
+    for c in cycles:
+        for i,v in enumerate(c[:-1]):
+            if (v == e[0]) and (c[i+1] == e[1]):
+                    path.append(c[i+1:]+c[1:i+1])
+                    break
+    # filter solution as a reconstructable genome from path
+    solution = set()
+    for p in path:
+        s = pair_genome_path(k,d,p)
+        if s:
+            solution.add(s)
+    return list(solution)
 
-fname = 'C:/Users/ngaude/Downloads/dataset_203_6.txt'
-kmers = list(l[:-1] for l in open(fname))[1:]
-genome = genome_reconstruction(kmers)
+def pair_genome_path(k,d,pairs):
+    '''    
+    Implement StringSpelledByGappedPatterns.
+    Input: An integer d followed by a sequence of (k, d)-mers 
+    (a1|b1), … , (an|bn) such that 
+    Suffix(ai|bi) = Prefix(ai+1|bi+1) for 1 ≤ i ≤ n-1.
+    Output: A string Text of length k + d + k + n - 1 such that 
+    the i-th (k, d)-mer in Text is equal to
+    (ai|bi)  for 1 ≤ i ≤ n (if such a string exists)
+    '''
+    # d is the gap size
+    # assumes that the number pairs is at least the gap size d
+    # otherwise, it is impossible to reconstruct a contiguous string.
+    assert len(pairs) >= d
+    prefix = genome_path(zip(*pairs)[0])
+    suffix = genome_path(zip(*pairs)[1])
+#    print 'prefix',prefix+'x'*(k+d)
+#    print 'suffix','x'*(k+d)+suffix
+    # check if prefix and suffix matches  
+    if prefix[k+d:] == suffix[:-k-d]:
+        return prefix + suffix[-k-d:] 
+    else:
+        # no matching genome found from pairs-path
+        return None
+
+def debruijn_from_pair(d,pairs):
+    '''
+    Construct the de Bruijn graph from a set of pairs with gap d.
+    Input: A collection of k-mers pair tupples.
+    Output: The adjacency list of the de Bruijn graph DeBruijn.
+    '''
+    g = []
+    # build a prefixing pattern dict
+    dprefix = {}
+    for (e,f) in pairs:
+        prefix = (e[:-1],f[:-1])
+        suffix = (e[1:],f[1:])
+        dprefix.setdefault(prefix, []).append(suffix)
+    # build lexicographically sorted adjacency list
+    for k in sorted(dprefix.keys()):
+        g.append( (k,sorted(dprefix[k])) )
+    return g
+
+#fname = 'C:/Users/ngaude/Downloads/StringReconstructionFromReadPairs.txt'
+fname = 'C:/Users/ngaude/Downloads/dataset_204_14.txt'
+lines = list(l for l in open(fname))
+k = int(lines[0].split(' ')[0])
+d = int(lines[0].split(' ')[1])
+pairs = map(lambda e: (e.split('|')[0].strip(), e.split('|')[1].strip()), lines[1:])
+genome = genomes_reconstruction_from_pair(k, d, pairs)
 with open(fname+'.out', "w") as f:
-        f.write(genome)
+        f.write(genome[0])
 
+assert genomes_reconstruction_from_pair(4, 2, [('GAGA','TTGA'),('TCGT','GATG'),('CGTG','ATGT'),('TGGT','TGAG'),('GTGA','TGTT'),('GTGG','GTGA'),('TGAG','GTTG'),('GGTC','GAGA'),('GTCG','AGAT')]) == ['GTGGTCGTGAGATGTTGA']     
+assert pair_genome_path(4,2,[('GACC','GCGC'),('ACCG','CGCC'),('CCGA','GCCG'),('CGAG','CCGG'),('GAGC','CGGA')]) == 'GACCGAGCGCCGGA'
+assert pair_composition(3,2,'TAATGCCATGGGATGTT') == [('AAT', 'CAT'), ('ATG', 'ATG'), ('ATG', 'ATG'), ('CAT', 'GAT'), ('CCA', 'GGA'), ('GCC', 'GGG'), ('GGG', 'GTT'), ('TAA', 'CCA'), ('TGC', 'TGG'), ('TGG', 'TGT')]
 assert genome_reconstruction(['CTTA','ACCA','TACC','GGCT','GCTT','TTAC'])=='GGCTTACCA'
 assert eulerian_path([(0,[2]),(1,[3]),(2,[1]),(3,[0,4]),(6,[3,7]),(7,[8]),(8,[9]),(9,[6])]) == [6, 7, 8, 9, 6, 3, 0, 2, 1, 3, 4]
 assert rotate_cycle(eulerian_cycle([(0,[3]), (1,[0]), (2,[1,6]), (3,[2]), (4,[2]), (5,[4]), (6,[5,8]), (7,[9]), (8,[7]), (9,[6])]),0) == [0, 3, 2, 6, 8, 7, 9, 6, 5, 4, 2, 1, 0]
@@ -280,6 +460,69 @@ assert overlap(['ATGCG','GCATG','CATGC','AGGCA','GGCAT']) == [('AGGCA', 'GGCAT')
 assert genome_path(['ACCGA','CCGAA','CGAAG','GAAGC','AAGCT']) == 'ACCGAAGCT'
 assert composition(5, 'CAATCCAAC') == ['AATCC', 'ATCCA', 'CAATC', 'CCAAC', 'TCCAA']
 
+# quizz chapter 4
+#
+#kmers = ['AAAT','AATG','ACCC','ACGC','ATAC','ATCA','ATGC','CAAA','CACC','CATA','CATC','CCAG','CCCA','CGCT','CTCA','GCAT','GCTC','TACG','TCAC','TCAT','TGCA']
+#print 'quizz',genome_reconstruction(kmers)
+#
+
+#us = universal_string_brute_force(3)
+#quizz = ['1000101110','1101000111','1100011011','0101001101','0101010100','0111010001']
+##quizz  = ['1100011011','0111010010','0101001101','0011101000','0111010001','1110001011']
+#for c in quizz:
+#    print c,c in us
+
+   
+#
+#kmers = ['GCGA','CAAG','AAGA','GCCG','ACAA','AGTA','TAGG','AGTA','ACGT','AGCC','TTCG','AGTT','AGTA','CGTA','GCGC','GCGA','GGTC','GCAT','AAGC','TAGA','ACAG','TAGA','TCCT','CCCC','GCGC','ATCC','AGTA','AAGA','GCGA','CGTA']
+#g = debruijn_from_kmer(kmers)
+#for (e,ee) in g:
+#    if ('GTA' in ee): 
+#        print e,'==>',ee
+#
+
+#pairs = [('ACC', 'ATA'),('ACT', 'ATT'),('ATA', 'TGA'),('ATT', 'TGA'),('CAC', 'GAT'),('CCG', 'TAC'),('CGA', 'ACT'),('CTG', 'AGC'),('CTG', 'TTC'),('GAA', 'CTT'),('GAT', 'CTG'),('GAT', 'CTG'),('TAC', 'GAT'),('TCT', 'AAG'),('TGA', 'GCT'),('TGA', 'TCT'),('TTC', 'GAA')]
+#
+#pairs = [('ACC','ATA'),
+#('ACT','ATT'),
+#('ATA','TGA'),
+#('ATT','TGA'),
+#('CAC','GAT'),
+#('CCG','TAC'),
+#('CGA','ACT'),
+#('CTG','AGC'),
+#('CTG','TTC'),
+#('GAA','CTT'),
+#('GAT','CTG'),
+#('GAT','CTG'),
+#('TAC','GAT'),
+#('TCT','AAG'),
+#('TGA','GCT'),
+#('TGA','TCT'),
+#('TTC','GAA')]
+#g = genomes_reconstruction_from_pair(3, 1, pairs)
+#print g
+
+
+##############################
+
+
+#fname = 'C:/Users/ngaude/Downloads/dataset_6206_7.txt'
+#lines = list(l for l in open(fname))
+#d = int(lines[0])
+#pairs = map(lambda e: (e.split('|')[0].strip(), e.split('|')[1].strip()), lines[1:])
+#genome = pair_genome_path(d,pairs)
+#with open(fname+'.out', "w") as f:
+#        f.write(genome)
+
+#p = pair_composition(3,2,'TAATGCCATGGGATGTT')
+#print  '('+')('.join(map(lambda e : e[0]+'|'+e[1], p))+')'
+
+#fname = 'C:/Users/ngaude/Downloads/dataset_203_6.txt'
+#kmers = list(l[:-1] for l in open(fname))[1:]
+#genome = genome_reconstruction(kmers)
+#with open(fname+'.out', "w") as f:
+#        f.write(genome)
 
 #fname = 'C:/Users/ngaude/Downloads/dataset_203_5.txt'
 #ladj = [ ( int(l.split(' -> ')[0]), map(int,l.split(' -> ')[1].split(',')) ) for l in open(fname) ]

b0b1b452b54483636c6156acb471d62280311561
ngaude <oktopela@yahoo.fr>
2015-04-20 01:40:03 +0200

    UPGMA finally fixed...
    still do not understood with the distance matrix iterative did not
    worked ....

--- bioinformatics-002/bioinformatics_chapter11.py
+++ bioinformatics-002/bioinformatics_chapter11.py
@@ -7,6 +7,8 @@ Created on Sun Apr 19 23:23:18 2015
 
 import numpy as np
 
+
+
 def upgma(n,D):
     """
     CODE CHALLENGE: Implement UPGMA.
@@ -14,6 +16,20 @@ def upgma(n,D):
     Output: An adjacency list for the ultrametric tree returned by UPGMA. Edge weights
     should be accurate to three decimal places.
     """
+
+    def clusters_distance():
+        n = len(clusters)
+        dc = np.zeros( (n,n), dtype = float )
+        for c1 in range(n):
+            for c2 in range(c1+1,n):
+                dc1c2 = 0.
+                for i in clusters[c1]:
+                    for j in clusters[c2]:
+                        dc1c2 += D[i,j]
+                dc1c2 = dc1c2 / (1.*len(clusters[c1])*len(clusters[c2]))
+                dc[c1,c2] = dc1c2
+                dc[c2,c1] = dc1c2
+        return dc    
     
     def find_closest_clusters():
         (n,m) = dclusters.shape
@@ -27,8 +43,19 @@ def upgma(n,D):
                     dmin = dclusters[i,j]
                     pmin = (i,j)
         return pmin
+
+    def merge_clusters_brute_force(c1,c2):
+        c = clusters[c1]+clusters[c2]
+        clusters.pop(c1)
+        clusters.pop(c2-1)
+        clusters.append(c)
+        nid = max(clusterid)+1
+        clusterid.pop(c1)        
+        clusterid.pop(c2-1)
+        clusterid.append(nid)
+        return clusters_distance()
         
-    def merge_clusters(c1,c2):
+    def merge_clusters_to_be_fixed(c1,c2):
         # return a new distance cluster matrix and update the clusters list...
         (n,m) = dclusters.shape
         assert n == m
@@ -39,27 +66,22 @@ def upgma(n,D):
         ii = 0
         jj = 0
         dmerged = np.zeros( (n-1,n-1), dtype = float )
-        print 'n-1',n-1
         for i in range(n):
             if i == c1 or i == c2:
                 continue
             for j in range(n):
                 if j == c1 or j == c2:
                     continue
-                print ii,jj
                 dmerged[ii,jj] = dclusters[i,j]
                 jj +=1
             ii +=1
             jj = 0
         # update last column and row with merged cluster
-        for i in range(n-1):
-            dc = (dclusters[c1,i]*len(clusters[c1]) + dclusters[c2,i]*len(clusters[c2]))/(len(clusters[c2])+len(clusters[c1]))
+        for i in range(n-2):
+            dc = (dclusters[c1,i]*len(clusters[c1]) + dclusters[c2,i]*len(clusters[c2]))/float(len(clusters[c2])+len(clusters[c1]))
             dmerged[n-2,i] = dc
             dmerged[i,n-2] = dc
         c = clusters[c1]+clusters[c2]
-        print 'clusters',clusters
-        print 'c1',c1
-        print 'c2',c2
         clusters.pop(c1)
         clusters.pop(c2-1)
         clusters.append(c)
@@ -89,40 +111,52 @@ def upgma(n,D):
         cid2 = clusterid[c2]        
         dc1c2 = dclusters[c1,c2]
         # merge C1 and C2 into a new cluster C
-        dclusters = merge_clusters(c1,c2)
+        dclusters = merge_clusters_brute_force(c1,c2)
         cid = clusterid[-1]
         # add a new node C to T and connect it to nodes C1 and C2 by directed edges
         T[cid] = [cid1,cid2]
         T[cid1].append(cid)
         T[cid2].append(cid)        
         # Age(C) ← DC1,C2 / 2
-        age[cid] = dc1c2/2
+        age[cid] = dc1c2/2.
+#        print '--------------------------------'
+#        print 'clusters',clusters
+#        print 'clusterid',clusterid
+#        print 'dclusters',dclusters
+#        print 'clusters_distance',clusters_distance(clusters,D)
+#        print 'age',age
+#        print '--------------------------'        
     adj = {}
-    print 'T',T
     for u,vs in T.iteritems():
         for v in vs:        
-            w = age[u] - age[v]
+            w = abs(age[u] - age[v])
             adj.setdefault(u,[]).append((v,w))
     return adj
-"""    
-    UPGMA(D, n)
- form n clusters, each containing a single element i (for i from 1 to n)
- construct a graph T by assigning a node to each cluster (without adding any edges) 
- for every node v in T 
-  Age(v) ← 0
- while there is more than one cluster
-  find two closest clusters C1 and C2 (break ties arbitrarily)
-  merge C1 and C2 into a new cluster C
-  add a new node C to T and connect it to nodes C1 and C2 by directed edges
-  Age(C) ← DC1,C2 / 2
-  remove rows and columns of D corresponding to C1 and C2
-  add a row and column to D for C by recomputing DC,C* for each C* ≠ C
- root ← the node in T corresponding to the cluster C
- for each edge (v,w) in T
-  Length(v,w) ← Age(v) - Age(w)
- return T
- """
  
 D = np.array([[0,20,17,11],[20,0,20,13],[17,20,0,10],[11,13,10,0]])
-adj = upgma(4,D)
-print adj
+T = upgma(4,D)
+res = {0: [(5, 7.0)], 1: [(6, 8.8333333333333339)], 2: [(4, 5.0)], 3: [(4, 5.0)], 4: [(2, 5.0), (3, 5.0), (5, 2.0)], 5: [(0, 7.0), (4, 2.0), (6, 1.8333333333333339)], 6: [(1, 8.8333333333333339), (5, 1.8333333333333339)]}
+assert T == res
+
+
+############################################################
+#fpath = 'C:/Users/ngaude/Downloads/'
+#fpath = '/home/ngaude/Downloads/'
+fpath = 'C:/Users/Utilisateur/Downloads/'
+############################################################
+
+fname = fpath + 'dataset_10332_8.txt'
+with open(fname, "r") as f:
+    lines = f.read().strip().split('\n')
+    n = int(lines[0])
+    d = np.zeros(shape=(n,n),dtype = float)    
+    for i,l in enumerate(lines[1:]):
+        for j,v in enumerate(map(int,l.split('\t')[:-1])):
+            d[i,j] = v
+#    d = map(lambda r: map(int, r.split('\t')[:-1]), lines[1:])
+#    d = np.array(d)
+T = upgma(n,d)
+with open(fname+'.out', "w") as f:
+   for u,vw in T.iteritems():
+       for (v,w) in vw:
+           f.write(str(u)+'->'+str(v)+':'+"{0:.3f}".format(w)+'\n')

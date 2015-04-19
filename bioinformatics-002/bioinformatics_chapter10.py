71012bb9bf8b484dbb6602293f9c424c2d5ad453
ngaude <oktopela@yahoo.fr>
2015-04-19 02:23:31 +0200

    chapter 10 completed : phylogeny !

--- bioinformatics-002/bioinformatics_chapter10.py
+++ bioinformatics-002/bioinformatics_chapter10.py
@@ -77,23 +77,187 @@ def distances_between_leaves(n,adjacency_list):
             leaf_to_leaf_distance[i,j] = node_to_node_distance[m,n]
     return leaf_to_leaf_distance
 
+m = distances_between_leaves(4,[(0, 4, 11), (1, 4, 2), (2, 5, 6), (3, 5, 7), (4, 0, 11), (4, 1, 2), (4, 5, 4), (5, 4, 4), (5, 3, 7), (5, 2, 6)])
+res = np.array([[ 0, 13, 21, 22],[13,  0, 12, 13],[21, 12,  0, 13],[22, 13, 13,  0]])
+assert not (m != res).sum()
+
+def limb_length(n, j, d):
+    """
+    CODE CHALLENGE: Solve the Limb Length Problem.
+    Input: An integer n, followed by an integer j between 0 and n, followed by a space-separated
+    additive distance matrix D (whose elements are integers).
+    Output: The limb length of the leaf in Tree(D) corresponding to the j-th row of this distance
+    matrix (use 0-based indexing).
+    """
+    assert n > 2
+    assert d.shape == (n,n)
+    i = (j+1) % n
+    k = (j+2) %n
+    minlen = (d[i,j] + d[j,k] - d[i,k])/2
+    for i in range(n):
+        for k in range(i+1,n):
+            if i==j or k==j:
+                continue
+            currlen = (d[i,j] + d[j,k] - d[i,k])/2
+            minlen = min(minlen,currlen)
+    return minlen
+
+def limb_length_linear(n, j, d):
+    """
+    CODE CHALLENGE: Solve the Limb Length Problem in linear time.
+    Input: An integer n, followed by an integer j between 0 and n, followed by a space-separated
+    additive distance matrix D (whose elements are integers).
+    Output: The limb length of the leaf in Tree(D) corresponding to the j-th row of this distance
+    matrix (use 0-based indexing).
+    """
+    assert n > 2
+    assert d.shape == (n,n)
+    i = (j+1) % n
+    k = (j+2) %n
+    minlen = (d[i,j] + d[j,k] - d[i,k])/2
+    for k in range(n):
+        if i==j or k==j:
+            continue
+        currlen = (d[i,j] + d[j,k] - d[i,k])/2
+        minlen = min(minlen,currlen)
+    return minlen
+
+assert map(lambda j:limb_length(4,j,np.array([[0,13,21,22],[13,0,12,13],[21,12,0,13],[22,13,13,0]])), range(4)) == [11,2,6,7]
+assert map(lambda j:limb_length_linear(4,j,np.array([[0,13,21,22],[13,0,12,13],[21,12,0,13],[22,13,13,0]])), range(4)) == [11,2,6,7]
 
 
-#s = '4\n0->4:11\n1->4:2\n2->5:6\n3->5:7\n4->0:11\n4->1:2\n4->5:4\n5->4:4\n5->3:7\n5->2:6'
-#lines = s.split('\n')
-#n = int(lines[0])
-#ladj = [ ( int(l.split('->')[0]), int(l.split('->')[1].split(':')[0]), int(l.split('->')[1].split(':')[1])) for l in lines[1:] ]
-#m = distances_between_leaves(n,ladj)
+def additive_phylogeny(n,d):
+    """
+    CODE CHALLENGE: Implement AdditivePhylogeny to solve the Distance-Based Phylogeny Problem.
+    Input: An integer n followed by a space-separated n x n distance matrix.
+    Output: A weighted adjacency list for the simple tree fitting this matrix.
+    """
+    def find_path(i,j):
+        visited = [None] * (max(adj.keys())+1)
+        def dfs(path):
+            for (v,w) in adj[path[-1][0]]:
+                if visited[v] == True:
+                    continue
+                visited[v] = True
+                pathlen = path[-1][1] + w
+                npath = path[:]                
+                npath.append((v,pathlen))
+                if (v == j):
+                    # found the node that ends path.
+                    return npath
+                result = dfs(npath)
+                if result is not None:
+                    return result
+            return
+        return dfs([(i,0)])
+        
+    def add_leaf(leaf,i,j,x,w):
+        # add leaf on the path from i to j,
+        # attached at x distance from i
+        # with weigth w
+        path = find_path(i,j)
+        # check if an x distant parent node already exists in path p
+        parent = None
+        for k in range(len(path)-1):
+            if path[k][1] == x:
+                # parent node already exists
+                parent = path[k][0]
+                break
+            if path[k][1] < x and x < path[k+1][1]:
+                # cut the u->v:w0 edge into
+                # u->parent:w1 , parent->v:w2
+                u = path[k][0]
+                v = path[k+1][0]
+                w0 = path[k+1][1] - path[k][1]
+                w1 = x - path[k][1]
+                w2 = w0 - w1
+                # find a new node slot
+                parent = max(adj.keys()+[n-1])+1 
 
-fname = 'C:/Users/ngaude/Downloads/dataset_10328_11.txt'
-with open(fname, "r") as f:
-    lines = f.read().strip().split('\n')
-    n = int(lines[0])
-    ladj = [ ( int(l.split('->')[0]), int(l.split('->')[1].split(':')[0]), int(l.split('->')[1].split(':')[1])) for l in lines[1:] ]
-    m = distances_between_leaves(n,ladj)
-with open(fname+'.out', "w") as f:
-   for row in m:
-       f.write(' '.join(map(str,row)))
-       f.write('\n')
+                adj[u].remove((v,w0))
+                adj[v].remove((u,w0))
+                adj[parent] = []
+                
+                adj[u].append((parent,w1))
+                adj[parent].append((u,w1))
 
+                adj[v].append((parent,w2))
+                adj[parent].append((v,w2))                
+
+                break
+        assert parent is not None
+
+        adj.setdefault(leaf,[]).append((parent,w))
+        adj[parent].append((leaf,w))        
+        return
     
+    def find_condition(d,l):
+        # (i,l,k) ← three leaves such that
+        # Di,k = Di,l + Dl,k
+        # i < k < l
+        for i in range(l):
+            for k in range(i+1,l):
+                if d[i,k] == d[i,l]+d[l,k]:
+                    return (i,k)
+        assert 0
+        return
+    
+    def recursive(nn):
+        if nn==2:
+            return
+        limb = limb_length_linear(nn,nn-1,d[:nn,:nn])
+        for j in range(nn-1):
+            d[nn-1, j] -= limb
+            d[j, nn-1] -= limb
+        (i, k) = find_condition(d,nn-1)
+        x = d[i,nn-1]
+        recursive(nn-1)
+        add_leaf(nn-1,i,k,x,limb)
+        return
+        
+    adj = {0: [(1,d[0,1]),],1: [(0,d[1,0]),]}
+    recursive(n)
+    return adj
+   
+d = np.array([[0,13,21,22],[13,0,12,13],[21,12,0,13],[22,13,13,0]])
+T = additive_phylogeny(4,d)
+assert T == {0: [(4, 11)], 1: [(4, 2)], 2: [(5, 6)], 3: [(5, 7)], 4: [(0, 11), (1, 2), (5, 4)], 5: [(4, 4), (2, 6), (3, 7)]}
+
+
+############################################################
+#fpath = 'C:/Users/ngaude/Downloads/'
+#fpath = '/home/ngaude/Downloads/'
+fpath = 'C:/Users/Utilisateur/Downloads/'
+############################################################
+
+#fname = fpath + 'dataset_10328_11.txt'
+#with open(fname, "r") as f:
+#    lines = f.read().strip().split('\n')
+#    n = int(lines[0])
+#    ladj = [ ( int(l.split('->')[0]), int(l.split('->')[1].split(':')[0]), int(l.split('->')[1].split(':')[1])) for l in lines[1:] ]
+#    m = distances_between_leaves(n,ladj)
+#with open(fname+'.out', "w") as f:
+#   for row in m:
+#       f.write(' '.join(map(str,row)))
+#       f.write('\n')
+
+#fname = fpath + 'dataset_10329_11.txt'
+#with open(fname, "r") as f:
+#    lines = f.read().strip().split('\n')
+#    n = int(lines[0])
+#    j = int(lines[1])
+#    d = map(lambda r: map(int, r.split(' ')), lines[2:])
+#    d = np.array(d)
+#print limb_length_linear(n,j,d)
+
+#fname = fpath + 'dataset_10330_6.txt'
+#with open(fname, "r") as f:
+#    lines = f.read().strip().split('\n')
+#    n = int(lines[0])
+#    d = map(lambda r: map(int, r.split(' ')), lines[1:])
+#    d = np.array(d)
+#T = additive_phylogeny(n,d)
+#with open(fname+'.out', "w") as f:
+#   for u,vw in T.iteritems():
+#       for (v,w) in vw:
+#           f.write(str(u)+'->'+str(v)+':'+str(w)+'\n')

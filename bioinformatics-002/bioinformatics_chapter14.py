f6ee666ac4f6245350cb8686ec96c10228aa1943
ngaude <oktopela@yahoo.fr>
2015-05-17 01:43:07 +0200

    hierarchical clustering completed

--- bioinformatics-002/bioinformatics_chapter14.py
+++ bioinformatics-002/bioinformatics_chapter14.py
@@ -52,39 +52,85 @@ c = soft_kmeans(k,b,dp)
 assert map(lambda t:map(lambda f:round(f,3),t),c) == [[1.662, 2.623], [1.075, 1.148]]
 
 
+
+def hierarchical_clustering(n,dm):
+    """
+    CODE CHALLENGE: Implement HierarchicalClustering.
+    Input: An integer n, followed by an n x n distance matrix.
+    Output: The result of applying HierarchicalClustering to this distance matrix (using Davg),
+    with each newly created cluster listed on each line.
+    """
+    c = [ [1+i,] for i in range(n)]
+    r = []
+    odm = dm
+    def cluster_distance(ca,cb,dm):
+        dab = 0.
+        for i in ca:
+            for j in cb:
+                dab += dm[i-1,j-1]
+        dab = dab / (1.*len(ca)*len(cb))
+        return dab
+    while len(dm)>2:
+        n = len(dm)
+        a,b = np.unravel_index(np.argmin(dm + np.eye(n)*dm.max()),(n,n))
+        ca = c[a][:]
+        cb = c[b][:]
+        cc = ca+cb      
+        c.remove(ca)
+        c.remove(cb)
+        c.append(cc)
+        dm = np.delete(dm,(a,b),0)
+        dm = np.delete(dm,(a,b),1)
+        ndm = np.zeros((n-1,n-1))
+        ndm[:-1,:-1] = dm
+        for i in range(n-2):
+            dicc = cluster_distance(c[i],cc,odm)
+            ndm[-1,i] = dicc
+            ndm[i,-1] = dicc        
+        r.append(cc)
+        dm = ndm
+    r.append(c[0]+c[1])
+    return r
+        
+        
+
+dm = ((0.00, 0.74, 0.85, 0.54, 0.83, 0.92, 0.89),
+      (0.74, 0.00, 1.59, 1.35, 1.20, 1.48, 1.55),
+      (0.85, 1.59, 0.00, 0.63, 1.13, 0.69, 0.73),
+      (0.54, 1.35, 0.63, 0.00, 0.66, 0.43, 0.88),
+      (0.83, 1.20, 1.13, 0.66, 0.00, 0.72, 0.55),
+      (0.92, 1.48, 0.69, 0.43, 0.72, 0.00, 0.80),
+      (0.89, 1.55, 0.73, 0.88, 0.55, 0.80, 0.00))
+dm = np.array(dm)
+r = hierarchical_clustering(len(dm),dm)
+assert r == [[4, 6], [5, 7], [3, 4, 6], [1, 2], [5, 7, 3, 4, 6], [1, 2, 5, 7, 3, 4, 6]]
+
+
 ############################################################
 fpath = 'C:/Users/ngaude/Downloads/'
 #fpath = '/home/ngaude/Downloads/'
 #fpath = 'C:/Users/Utilisateur/Downloads/'
 ############################################################
 
-fname = fpath + 'dataset_10933_7.txt'
-with open(fname, "r") as f:
-    lines = f.read().strip().split('\n')
-    k = int(lines[0].split(' ')[0])
-    b = float(lines[1])
-    dp = map(lambda l:tuple(map(float,l.split(' '))),lines[2:])
-    c = soft_kmeans(k,b,dp)
-with open(fname+'.out', "w") as f:
-    for ci in c:
-        f.write(' '.join(map(str,ci))+'\n')
-
-#fname = fpath + 'SquaredErrorDistortion.txt'
-#fname = fpath + 'dataset_10927_3.txt'
+#fname = fpath + 'dataset_10933_7.txt'
 #with open(fname, "r") as f:
 #    lines = f.read().strip().split('\n')
 #    k = int(lines[0].split(' ')[0])
-#    c = map(lambda l:tuple(map(float,l.split(' '))),lines[1:k+1])
-#    dp = map(lambda l:tuple(map(float,l.split(' '))),lines[k+2:])
-#print squared_error_distortion(c,dp)
-
-#fname = fpath + 'Lloyd.txt'
-#fname = fpath + 'dataset_10928_3.txt'
-#with open(fname, "r") as f:
-#    lines = f.read().strip().split('\n')
-#    k = int(lines[0].split(' ')[0])
-#    dp = map(lambda l:tuple(map(float,l.split(' '))),lines[1:])
-#    c = lloyd_kmeans(k,dp)
+#    b = float(lines[1])
+#    dp = map(lambda l:tuple(map(float,l.split(' '))),lines[2:])
+#    c = soft_kmeans(k,b,dp)
 #with open(fname+'.out', "w") as f:
 #    for ci in c:
-#        f.write(' '.join(map(str,ci))+'\n')    
+#        f.write(' '.join(map(str,ci))+'\n')
+
+#fname = fpath + 'dataset_10934_7.txt'
+##fname = fpath + 'HierarchicalClustering.txt'
+#with open(fname, "r") as f:
+#    lines = f.read().strip().split('\n')
+#    n = int(lines[0].split(' ')[0])
+#    dm = map(lambda l:map(float,l.split(' ')),lines[1:])
+#    dm = np.array(dm)
+#    r = hierarchical_clustering(n,dm)
+#with open(fname+'.out', "w") as f:
+#    for ri in r:
+#        f.write(' '.join(map(str,ri))+'\n')

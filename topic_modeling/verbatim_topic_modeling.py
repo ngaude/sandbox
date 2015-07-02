dc55bfcb20b3ce41523a6c09549812ada50cf136
ngaude <oktopela@yahoo.fr>
2015-07-02 02:28:02 +0200

    add top-ten topics

--- topic_modeling/verbatim_topic_modeling.py
+++ topic_modeling/verbatim_topic_modeling.py
@@ -119,6 +119,17 @@ print(next(iter(mm_corpus)))
 
 _ = lda_model.print_topics(-1)  # print a few most important words for each LDA topic
 
+#INFO:gensim.models.ldamodel:topic #0 (0.100): 0.029*problème + 0.026*box + 0.024*internet + 0.020*wifi + 0.020*réseau + 0.017*téléphone + 0.016*fonctionne + 0.012*connexion + 0.009*jours + 0.007*voyant
+#INFO:gensim.models.ldamodel:topic #1 (0.100): 0.050*débit + 0.039*ttl + 0.037*temps + 0.024*octets + 0.022*descendant + 0.018*montant + 0.018*125 + 0.016*194 + 0.014*173 + 0.014*local
+#INFO:gensim.models.ldamodel:topic #2 (0.100): 0.039*sim + 0.032*carte + 0.022*téléphone + 0.020*commande + 0.016*service + 0.014*reçu + 0.013*nouvelle + 0.012*iphone + 0.012*boutique + 0.011*demande
+#INFO:gensim.models.ldamodel:topic #3 (0.100): 0.021*problème + 0.019*forum + 0.013*aide + 0.011*aider + 0.010*venir + 0.009*regrette + 0.008*questions + 0.008*service + 0.008*cas + 0.007*soirée
+#INFO:gensim.models.ldamodel:topic #4 (0.100): 0.034*message + 0.027*privé + 0.023*décodeur + 0.018*prise + 0.017*box + 0.013*indésirables + 0.013*suite + 0.012*adresse + 0.011*réception + 0.010*confidentialité
+#INFO:gensim.models.ldamodel:topic #5 (0.100): 0.059*numéro + 0.026*pouvez + 0.025*téléphone + 0.025*code + 0.023*mobile + 0.014*nom + 0.014*communiquer + 0.012*cordialement + 0.012*portable + 0.010*imei
+#INFO:gensim.models.ldamodel:topic #6 (0.100): 0.046*mobile + 0.043*www + 0.043*bouyguestelecom + 0.031*adresse + 0.029*mail + 0.022*mms + 0.021*assistance + 0.017*lien + 0.015*envoyer + 0.014*pouvez
+#INFO:gensim.models.ldamodel:topic #7 (0.100): 0.020*faut + 0.019*voir + 0.015*passe + 0.013*bonnes + 0.013*fêtes + 0.011*from + 0.011*time + 0.011*fibre + 0.011*application + 0.010*attendre
+#INFO:gensim.models.ldamodel:topic #8 (0.100): 0.030*retour + 0.027*part + 0.022*cordialement + 0.022*disposition + 0.022*réponse + 0.020*reste + 0.019*besoin + 0.018*fil + 0.017*colis + 0.016*permets
+#INFO:gensim.models.ldamodel:topic #9 (0.100): 0.032*mois + 0.029*offre + 0.025*facture + 0.015*you + 0.013*service + 0.013*euros + 0.012*remise + 0.010*2014 + 0.010*sensation + 0.009*mobile
+
 
 #### PERSIST AGAIN MODEL
 
